9bc08a3ca24fe02c0626bf228b1c8ff61dcb12db
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-09-06 21:31:42 +0700

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -1,1 +1,7 @@
-export AXFUN="anjay";
+export AX_PKG="!axPkg"
+export AX_ID="!axId"
+export AX_VNAME="!axVName"
+export AX_VCODE="!axVCode"
+export AX_MODULES_PATH="!axModules"
+export MODULE_PATH=""
+export AX_FUN="anjay";
