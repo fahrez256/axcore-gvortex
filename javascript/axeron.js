690f4f99d340eab00841392c7f3639939a0ff7f0
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-09-07 19:51:21 +0700

    Update axeron.js

--- javascript/axeron.js
+++ javascript/axeron.js
@@ -1,42 +1,51 @@
-// Import atau definisikan fungsi exec
-export function exec(command, options) {
-  if (typeof options === "undefined") {
-    options = {};
+class Axeron {
+  exec(command, options) {
+    if (typeof options === "undefined") {
+      options = {};
+    }
+
+    return new Promise((resolve, reject) => {
+      const callbackFuncName = getUniqueCallbackName("exec");
+
+      // Define the success callback function
+      window[callbackFuncName] = (errno, stdout, stderr) => {
+        resolve({ errno, stdout, stderr }); // Resolusi Promise dengan hasil dari Java
+        cleanup(callbackFuncName); // Hapus callback setelah digunakan
+      };
+
+      // Membersihkan callback setelah digunakan
+      function cleanup(successName) {
+        delete window[successName];
+      }
+
+      try {
+        // Memanggil fungsi Java melalui interface Android
+        Axeron.exec(command, JSON.stringify(options), callbackFuncName);
+      } catch (error) {
+        reject(error);
+        cleanup(callbackFuncName);
+      }
+    });
   }
 
-  return new Promise((resolve, reject) => {
-    const callbackFuncName = getUniqueCallbackName("exec");
+  // Fungsi untuk menghasilkan nama callback unik
+  getUniqueCallbackName(prefix) {
+    return (
+      prefix +
+      "_" +
+      new Date().getTime() +
+      "_" +
+      Math.floor(Math.random() * 1000)
+    );
+  }
 
-    // Define the success callback function
-    window[callbackFuncName] = (errno, stdout, stderr) => {
-      resolve({ errno, stdout, stderr });  // Resolusi Promise dengan hasil dari Java
-      cleanup(callbackFuncName);  // Hapus callback setelah digunakan
-    };
+  toast(message) {
+    Axeron.showToast(message);
+  }
 
-    // Membersihkan callback setelah digunakan
-    function cleanup(successName) {
-      delete window[successName];
-    }
-
-    try {
-      // Memanggil fungsi Java melalui interface Android
-      Axeron.exec(command, JSON.stringify(options), callbackFuncName);
-    } catch (error) {
-      reject(error);
-      cleanup(callbackFuncName);
-    }
-  });
+  optimizeApp(package) {
+    Axeron.optimizeApp(package);
+  }
 }
 
-// Fungsi untuk menghasilkan nama callback unik
-function getUniqueCallbackName(prefix) {
-  return prefix + "_" + new Date().getTime() + "_" + Math.floor(Math.random() * 1000);
-}
-
-export function toast(message) {
-	Axeron.showToast(message);
-}
-
-export function optimizeApp(package) {
-	Axeron.optimizeApp(package);
-}
+export const Axeron = new Main();
