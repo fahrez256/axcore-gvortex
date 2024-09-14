24567bd00d4fc68964c6a544c3830597a67ec7f2
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-09-14 07:57:25 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -32,3 +32,38 @@ pkglist() {
             ;;
     esac
 }
+
+toast() {
+	case $# in
+		1)
+			title=""
+			msg="$1"
+			duration=3000
+			;;
+		2)
+			case $2 in
+				''|*[!0-9]*)
+					title="$1"
+					msg="$2"
+					duration=0
+					;;
+				*)
+					title=""
+					msg="$1"
+					duration="$2"
+					;;
+			esac
+			;;
+		3)
+			title="$1"
+			msg="$2"
+			duration="$3"
+			;;
+		*)
+			echo "Usage: toast <msg> | toast <title> <msg> | toast <msg> <duration> | toast <title> <msg> <duration>"
+			return 1
+			;;
+	esac
+
+	am broadcast -a gvr.service.TOAST --es title "$title" --es message "$msg" --ei duration "$duration" > /dev/null 2>&1
+}
