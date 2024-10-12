fe9ad356d773415bc974c9cec341f15010019ba7
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-10-12 22:32:58 +0700

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -1,8 +1,10 @@
 export AX_PKG="!axPkg"
+export AX_CORE=$(dumpsys package "$AX_PKG" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 export AX_ID="!axId"
 export AX_VNAME="!axVName"
 export AX_VCODE="!axVCode"
 export GVR_PKG="!gvrPkg"
+export GVR_CORE=$(dumpsys package "$GVR_PKG" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 export GVR_VNAME="!gvrVName"
 export GVR_VCODE="!gvrVCode"
 export AX_MODULES_PATH="!axModulePath"
