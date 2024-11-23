5938c331337a2823a8bfa7343bdc42fa977d7566
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-11-23 16:49:45 +0700

    Create axConstant.js

---
+++ javascript/axConstant.js
@@ -0,0 +1,19 @@
+class AxConstant {
+  constructor() {
+    this.AX_PKG = "!axPkg";
+    this.AX_ID = "!axId";
+    this.AX_VNAME = "!axVName";
+    this.AX_VCODE = "!axVCode";
+    this.GVR_PKG = "!gvrPkg";
+    this.GVR_VNAME = "!gvrVName";
+    this.GVR_VCODE = "!gvrVCode";
+    this.AX_MODULES_PATH = "!axModulePath";
+
+    this.MODULE_PKG = "!modulePkg";
+    this.MODULE_NAME = "!moduleName";
+    this.MODULE_PATH = `${this.AX_MODULES_PATH}/${this.MODULE_NAME}`;
+    this.MODULE_PATH_WEB = `${this.AX_MODULES_PATH}/${this.MODULE_NAME}/webroot`;
+  }
+}
+
+export default new AxConstant();
