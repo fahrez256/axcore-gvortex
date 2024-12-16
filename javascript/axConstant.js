5ab5ae9ae0657775327353084ec32edd6a378dc7
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-12-16 21:38:18 +0700

    Update axConstant.js

--- javascript/axConstant.js
+++ javascript/axConstant.js
@@ -11,10 +11,10 @@ class AxConstant {
 
     this.MODULE_PKG = "!modulePkg";
     this.MODULE_PKG_VNAME = "!pkgVName";
-    this.MODULE_PKG_VCODE = !pkgVCode;
+    this.MODULE_PKG_VCODE = "!pkgVCode";
     this.MODULE_NAME = "!moduleName";
     this.MODULE_VNAME = "!moduleVName";
-    this.MODULE_VCODE = !moduleVCode;
+    this.MODULE_VCODE = "!moduleVCode";
     this.MODULE_PATH = `${this.AX_MODULES_PATH}/${this.MODULE_NAME}`;
     this.MODULE_PATH_WEB = `${this.AX_MODULES_PATH}/${this.MODULE_NAME}/webroot`;
     this.MODULE_PATH_ICON = `/${this.MODULE_NAME}/vmods.png`;
